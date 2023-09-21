b325b7980c85a19c1240aa90fd3c5526afb9d7af
SaJH <wogur981208@gmail.com>
2023-09-21 17:17:28 +0900

    Fix: app 실행 버그 수정

--- euss_ya/exercises/apps.py
+++ euss_ya/exercises/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExercisesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "exercises"
+    name = "euss_ya.exercises"
